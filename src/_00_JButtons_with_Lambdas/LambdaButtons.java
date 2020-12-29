e24db4a265716cbc124f7945fc8a40eaa84a019c
11002084 <36611325+11002084@users.noreply.github.com>
2020-12-29 15:10:59 -0800

    12/29/2020
    Finished Lambda Buttons

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,11 +1,15 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
-public class LambdaButtons {
+public class LambdaButtons implements ActionListener {
 	private JFrame window = new JFrame();
 	private JButton addNumbers = new JButton("ADD 2 Numbers");
 	private JButton randNumber = new JButton("RANDOM NUMBER");
@@ -20,6 +24,9 @@ public class LambdaButtons {
 		
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
+		addNumbers.addActionListener(this);
+		randNumber.addActionListener(this);
+		tellAJoke.addActionListener(this);
 		
 		window.setVisible(true);
 		window.pack();
@@ -28,6 +35,34 @@ public class LambdaButtons {
 	public static void main(String[] args) {
 		new LambdaButtons();
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == addNumbers) {
+			AddButton addButton = (int a, int b) -> a + b;
+			JOptionPane.showMessageDialog(null, addButton.addition(
+					Integer.parseInt(JOptionPane.showInputDialog("First Number:")), 
+					Integer.parseInt(JOptionPane.showInputDialog("Second Number"))
+			));
+		} else if(e.getSource() == randNumber) {
+			RandNumberButton randomNumberButton = (Random rand) -> rand.nextInt();
+			JOptionPane.showMessageDialog(null, randomNumberButton.random(new Random()));
+		} else if(e.getSource() == tellAJoke) {
+			JokeButton jokeButton = () -> "Why did the chicken cross the road? To get to the other side!";
+			JOptionPane.showMessageDialog(null, jokeButton.joke());
+		}
+	}
 	
+	interface AddButton {
+		int addition(int a, int b);
+	}
 	
+	interface RandNumberButton {
+		int random(Random rand);
+	}
+	
+	interface JokeButton {
+		String joke();
+	}
+
 }

4b3af97601958987aeb759357850cd8a400b0698
11002084 <36611325+11002084@users.noreply.github.com>
2020-12-30 15:00:52 -0800

    12/30/2020
    Finished Lambda Methods and Creating Streams

--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -13,17 +13,54 @@ public class LambdaMethods {
 		}, "repeat");
 		
 		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
+		printCustomMessage((s)->{
+			for(int i = s.length() - 1; i >= 0; i--) {
+				System.out.print(s.charAt(i));
+			}
+		}, "backward");
 		
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
+		printCustomMessage((s)->{
+			for(int i = 0; i < s.length(); i++) {
+				if(i%2 == 0) {
+					System.out.print(Character.toUpperCase(s.charAt(i)));
+				} else {
+					System.out.print(Character.toLowerCase(s.charAt(i)));
+				}
+			}
+		}, "upper and lower");
 		
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
+		printCustomMessage((s)->{
+			for(int i = 0; i < s.length(); i++) {
+				System.out.print(s.charAt(i));
+				if(i < s.length() - 1) {
+					System.out.print(".");
+				}
+			}
+		}, "periods");
 		
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
-	
+		printCustomMessage((s)->{
+			for(int i = 0; i < s.length(); i++) {
+				if(!isVowel(s.charAt(i))) {
+					System.out.print(s.charAt(i));
+				}
+			}
+		}, "no vowels abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 	}
 	
 	public static void printCustomMessage(SpecialPrinter sp, String value) {
 		sp.printSpecial(value);
+		System.out.print("\n");
+	}
+	
+	public static boolean isVowel(char c) {
+		if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
 	
